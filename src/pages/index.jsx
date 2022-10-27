f4c5c90a3e6e8b95ba331738342ac93d25828d91
Yusei Morimoto <yusei0509@icloud.com>
2022-10-27 15:48:34 +0900

    🎨 Custom Hooksを使ってまとめる

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -3,49 +3,14 @@ import styles from "src/styles/Home.module.css";
 import { Footer } from "src/components/Footer";
 import { Header } from "src/components/Header";
 import { Main } from "src/components/Main";
-import { useCallback, useEffect, useState } from "react";
+import { useCounter } from "src/hooks/useCounter";
+import { useInputArray } from "src/hooks/useInputArray";
+import { useBgLightBlue } from "src/hooks/useBgLightBlue";
 
 export default function Home() {
-  const [count, setCount] = useState(1);
-  const [text, setText] = useState("");
-  const [isShow, setIsShow] = useState(true);
-  const [array, setArray] = useState([]);
-
-  const handleClick = useCallback(() => {
-    console.log(count);
-    if (count < 10) {
-      setCount((prevCount) => prevCount + 1);
-    }
-  }, [count]);
-
-  const handleDisplay = useCallback(() => {
-    setIsShow((prevIsShow) => !prevIsShow);
-  }, []);
-
-  const hadleChange = useCallback((e) => {
-    if (e.target.value.length > 5) {
-      alert("5文字以内にしてください");
-      return;
-    }
-    setText(e.target.value.trim());
-  }, []);
-
-  const handleAdd = useCallback(() => {
-    setArray((prevArray) => {
-      if (prevArray.some((item) => item === text)) {
-        alert("同じ要素が既に存在します");
-        return prevArray;
-      }
-      return [...prevArray, text];
-    });
-  }, [text]);
-
-  useEffect(() => {
-    document.body.style.backgroundColor = "lightblue";
-    return () => {
-      document.body.stylecountgroundColor = "";
-    };
-  }, [count]);
+  const { count, isShow, handleClick, handleDisplay } = useCounter();
+  const { text, array, hadleChange, handleAdd } = useInputArray();
+  useBgLightBlue();
 
   return (
     <div className={styles.container}>
@@ -53,6 +18,7 @@ export default function Home() {
         <title>Index Page</title>
       </Head>
       <Header />
+
       {isShow ? <h1>{count}</h1> : null}
       <button onClick={handleClick}>ボタン</button>
       <button onClick={handleDisplay}>{isShow ? "非表示" : "表示"}</button>
